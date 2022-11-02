d9cf4535d0b096cea4926242debfc9c083cd0157
cwy <834415728@qq.com>
2022-11-03 00:27:15 +0800

    feat

---
+++ vue_test/16_src_TodoList_nextTick/App.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="home">
+		<div id="box">
+			<div v-for="(item, index) in boxArray" :key="index">
+				{{ item }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				boxArray: [1, 2, 3],
+			};
+		},
+		mounted() {
+			this.boxArray = [4, 5, 6, 7, 8, 9];
+			this.$nextTick(() => {
+				console.log('所有box的宽度为：', getComputedStyle(box).width)
+			});
+		},
+	};
+</script>
+
+<style>
+#box {
+	border: 1px solid red;
+	display: inline-block;
+}
+#box > div {
+	display: inline-block;
+	width: 100px;
+	height: 100px;
+}
+</style>
